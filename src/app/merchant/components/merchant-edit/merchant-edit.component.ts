2452e0214265628ac7ea5002dc5d00271f2e21ce
劉彥治 <ct0126@gmail.com>
2020-10-04 16:15:23 +0800

    新增編輯商家資料功能

--- src/app/merchant/components/merchant-edit/merchant-edit.component.ts
+++ src/app/merchant/components/merchant-edit/merchant-edit.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+import { ClonerService } from './../../../core/services/cloner.service';
+import { Merchant } from '../../models/merchant.model';
 
 @Component({
   selector: 'app-merchant-edit',
   templateUrl: './merchant-edit.component.html',
-  styleUrls: ['./merchant-edit.component.scss']
+  styleUrls: ['./merchant-edit.component.scss'],
 })
 export class MerchantEditComponent implements OnInit {
+  merchant = new Merchant();
 
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<MerchantEditComponent>,
+    @Inject(MAT_DIALOG_DATA) public data,
+    private clonerSevice: ClonerService
+  ) {}
 
   ngOnInit(): void {
+    if (this.data.merchant) {
+      this.merchant = this.clonerSevice.deepClone(this.data.merchant);
+    }
   }
 
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
--- src/app/merchant/containers/merchant-list/merchant-list.component.ts
+++ src/app/merchant/containers/merchant-list/merchant-list.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
+
+import { MerchantsService } from './../../services/merchants.service';
 import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { take } from 'rxjs/operators';
 import { Merchant } from '../../models/merchant.model';
-import { MerchantsService } from '../../services/merchants.service';
+import { MerchantEditComponent } from '../../components/merchant-edit/merchant-edit.component';
 
 @Component({
   selector: 'app-merchant-list',
@@ -12,26 +15,61 @@ import { MerchantsService } from '../../services/merchants.service';
 export class MerchantListComponent implements OnInit {
   merchants$: Observable<Merchant[]>;
 
-  constructor(private merchantsService: MerchantsService) {}
+  constructor(
+    private merchantsService: MerchantsService,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.merchants$ = this.merchantsService.getMerchants();
   }
 
-  openEditModal(mode, merchantId?): void {
-    console.log('mode', mode);
-    console.log('id', merchantId);
+  openEditModal(mode: 'create' | 'edit', merchantId?: string): void {
+    let dialogRef;
+    const modalComponent = MerchantEditComponent;
+    const modalWith = '500px';
+    if (merchantId) {
+      this.merchantsService
+        .getMerchantById(merchantId)
+        .pipe(take(1))
+        .subscribe((merchant) => {
+          dialogRef = this.dialog.open(modalComponent, {
+            width: modalWith,
+            data: { mode, merchant },
+          });
+        });
+    } else {
+      dialogRef = this.dialog.open(modalComponent, {
+        width: modalWith,
+        data: { mode },
+      });
+    }
+
+    dialogRef
+      .afterClosed()
+      .pipe(take(1))
+      .subscribe((merchant: Merchant) => {
+        if (!merchant) return;
+        switch (mode) {
+          case 'create':
+            this.createMerchant(merchant);
+            break;
+          case 'edit':
+            this.updateMerchant(merchant);
+            break;
+        }
+      });
   }
 
-  createMerchant(merchant): void {
+  createMerchant(merchant: Merchant): void {
     this.merchantsService.createMerchant(merchant);
   }
 
-  updateMerchant(merchant): void {
+  updateMerchant(merchant: Merchant): void {
     this.merchantsService.updateMerchant(merchant);
   }
 
-  deleteMerchant(merchantId): void {
+  deleteMerchant(merchantId: Merchant): void {
     this.merchantsService.deleteMerchant(merchantId);
   }
 }
